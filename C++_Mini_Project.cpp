1dd26f51f65e41e4a05bc18ee98922f438a268d3
Aarya Shah <148608350+shahaarya465@users.noreply.github.com>
2024-03-27 13:03:34 +0530

    Adding main function

--- C++_Mini_Project.cpp
+++ C++_Mini_Project.cpp
@@ -2,5 +2,24 @@
 #include <cstdlib>
 int main()
 {
+    srand(time(0));
+    int choice;
+    cout<<"Welcome to the Game Menu!" << endl;
+    cout<<"1. Play Casino Game" << endl;
+    cout<<"2. Play Rock-Paper-Scissors Game" << endl;
+    cout<<"Enter 1 for Casino Game and 2 for Rock-Paper-Scissors Game: ";
+    cin>>choice;
+    switch (choice)
+        {
+            case 1:
+                playCasino();
+                break;
+            case 2:
+                playRockPaperScissors();
+                break;
+            default:
+                cout<<"Invalid choice! Exiting..." << endl;
+                break;
+        }
     return 0;
 }
