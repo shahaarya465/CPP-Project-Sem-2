309013b57004d1b621b811f5419437350278f2e4
vansh1-0 <148608357+vansh1-0@users.noreply.github.com>
2024-03-27 20:48:57 +0530

    casino game funtion defination

--- C++_Mini_Project.cpp
+++ C++_Mini_Project.cpp
@@ -11,6 +11,51 @@ int rand_num()
     return rand() % 10 + 1;
 }
 
+void playCasino() 
+{
+    long long amount_money;
+    cout << "Enter Deposit amount to play game: $";
+    cin >> amount_money;
+    while (true) 
+    {
+        long long betting_amount;
+        if (amount_money == 0) 
+        {
+            cout << "Sorry, you have lost all your money. Your balance now is $0." << endl;
+            return;
+        }
+        cout << "Enter betting amount: ";
+        cin >> betting_amount;
+        if (betting_amount > amount_money) 
+        {
+            cout << "You don't have enough money." << endl;
+            continue;
+        }
+        cout << "Guess a number between 1 and 10: ";
+        int n;
+        cin >> n;
+        if (n > 10 || n <= 0) 
+        {
+            cout << "Wrong number, try again." << endl;
+            continue;
+        } else 
+        {
+            int r = rand_num();
+            if (r == n) 
+            {
+                amount_money += (betting_amount * 10);
+                cout << "Winner! Your balance now is $" << amount_money << endl;
+            } else 
+            {
+                amount_money -= betting_amount;
+                cout << "Loser! Your balance now is $" << amount_money << endl;
+                cout << "The Number was: " << r << endl;
+            }
+        }
+    }
+}
+
+
 int main()
 {
     srand(time(0));
